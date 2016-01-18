2011661240683cba384126a7cb449bc7f33b9ad3
Denis Susloparov <densus@gmail.com>
2016-01-18 07:13:20 +0600

    new carousel, better loading, better mobile support

--- js/site.js
+++ js/site.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+$("document").ready(function() {
 
-  $(".owl-carousel").owlCarousel({
-    items : 1,
-    lazyLoad : true,
-  });
-
+  $('.modal').on('show.bs.modal', function(e) {
+    var carousel = $(e.currentTarget).find('.carousel').first();
+    if (carousel.hasClass("slick-initialized"))
+      return;
+    carousel.slick({
+      dots: true,
+      speed: 500,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      lazyLoad: 'progressive',
+      arrows: false,
+    });
+  })
 });
