3ab97aa97be77d8720a7d395852bfd4dbd923c0e
Denis <densus@gmail.com>
2016-01-29 22:19:15 +0600

    better tablet support

--- js/site.js
+++ js/site.js
@@ -12,5 +12,8 @@ $("document").ready(function() {
       lazyLoad: 'progressive',
       arrows: false,
     });
+    carousel.css({
+     minHeight: $( window ).width()/2.15 +"px",
+    });
   })
 });
